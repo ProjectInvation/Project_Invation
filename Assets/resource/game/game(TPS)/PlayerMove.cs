21396f77f453d925f8ad16e61d29671e9c5766ad
hakata02 <remifurataku@gmail.com>
2015-03-09 01:18:05 +0900

    TPS Scene:  resource PlayerMove,CameraMove Create ModelPack load

---
+++ Assets/resource/game/game(TPS)/PlayerMove.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+	private Vector3 pos;
+	private Vector3 rot;
+	private Vector3 Front;
+	private Vector3 Up;
+	private Vector3 Right;
+
+	// Use this for initialization
+	void Start () {
+		pos.x = 0.0f;
+		pos.y = 1.0f;
+		pos.z = 0.0f;
+
+		this.transform.position = pos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Front = this.transform.forward;
+		Up = this.transform.up;
+		Right = this.transform.right;
+
+		//AllVector is Normalize
+		Front.Normalize();
+		Up.Normalize();
+		Right.Normalize();
+
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			pos = pos + (Front / 5);
+		}else if(Input.GetKey(KeyCode.S)){
+			pos = pos + -(Front / 5);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			rot.y++;
+		} else if (Input.GetKey (KeyCode.A)) {
+			rot.y--;
+		} else {
+			rot.y = 0;
+		}
+
+		this.transform.Rotate(rot);
+		this.transform.position = pos;
+	}
+}
