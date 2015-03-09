28be7b1d5bcd89aab6a35126d5c7cbffa8018e5c
sakamotokun <sw.style0312@gmail.com>
2015-03-09 20:53:40 +0900

    フィールドの作成、プレイヤーの簡単な移動処理、簡単な当たり判定実装、まだ全て途中

---
+++ Assets/scenes/game/game(FIX)/Scripts/Fix_PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fix_PlayerMove : MonoBehaviour 
+{
+	//Player Movement Spd
+	public float move_spd=0.0001f;
+	private GameObject Player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Player=GameObject.Find("Army 01");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetKey(KeyCode.W))
+		{
+			Player.transform.Translate(0,0,move_spd);
+		}
+
+		if(Input.GetKey(KeyCode.S))
+		{
+			Player.transform.Translate(0,0,-move_spd);
+		}
+
+		if(Input.GetKey(KeyCode.A))
+		{
+			Player.transform.Translate(-move_spd,0,0);
+		}
+
+		if(Input.GetKey(KeyCode.D))
+		{
+			Player.transform.Translate(move_spd,0,0);
+		}
+
+	}
+}
