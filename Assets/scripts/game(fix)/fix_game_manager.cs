1a4d4eaafcf1dd09d382e45a8e8c928513d209ef
sakamotokun <sw.style0312@gmail.com>
2015-03-11 12:30:12 +0900

    固定カメラ機能の追加

---
+++ Assets/scripts/game(fix)/fix_game_manager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fix_game_manager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
