ebc38310d7b0939d1211d5b3095dd70795f521aa
sakamotokun <sw.style0312@gmail.com>
2015-03-14 23:19:14 +0900

    アイテム取得処理の追加

---
+++ Assets/scripts/item_ui/Item/Item.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour
+{
+	int thisItemID=0;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void OnCollisionEnter(Collision coll)
+	{
+		if(coll.collider.tag=="Player")
+		{
+			if(GameObject.Find("ItemManager").GetComponent<ItemManager>().AddItem(thisItemID))
+			{
+				Destroy(this.gameObject);
+			}
+		}
+	}
+	
+}
