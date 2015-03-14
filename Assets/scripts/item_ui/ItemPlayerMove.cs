1ae6216d759c23d2ee259d2c65d9673cce7aa319
sakamotokun <sw.style0312@gmail.com>
2015-03-14 15:58:19 +0900

    調整

---
+++ Assets/scripts/item_ui/ItemPlayerMove.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemPlayerMove : MonoBehaviour
+{
+	public float MoveSpd;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//Move This Object
+		Move();
+
+		//Renderer Off
+		//GetComponent<Renderer>().enabled = false;
+	}
+
+	void Move()
+	{
+		if(Input.GetKey(KeyCode.W))
+		{
+			GameObject.Find("Player").transform.Translate(0,0,MoveSpd);
+		}
+		
+		if(Input.GetKey(KeyCode.S))
+		{
+			GameObject.Find("Player").transform.Translate(0,0,-MoveSpd);
+		}
+		
+		if(Input.GetKey(KeyCode.A))
+		{
+			GameObject.Find("Player").transform.Translate(-MoveSpd,0,0);
+		}
+		
+		if(Input.GetKey(KeyCode.D))
+		{
+			GameObject.Find("Player").transform.Translate(MoveSpd,0,0);
+		}
+	}
+}
