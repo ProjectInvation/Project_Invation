8525a493a0d2a568ffb63d65805a6c3c5e883b4e
HOSSY0909 <star.and.river.k.h@i.softbank.jp>
2015-03-18 12:54:10 +0900

    女性model追加、field編集

---
+++ Assets/scripts/hos_field/hos_test_camera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hos_test_camera : MonoBehaviour {
+
+	Camera Camera1;//カメラ1
+	Camera Camera2;//カメラ2
+
+	// Use this for initialization
+	void Start () {
+		Camera1 = GameObject.Find("Camera1").GetComponent<Camera>();
+		Camera2 = GameObject.Find ("Camera2").GetComponent<Camera> ();
+		
+		Camera2.enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Spaceキーで切り替え
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			if(Camera1.enabled){
+				Camera1.enabled = false;
+				Camera2.enabled = true;
+			}else{
+				Camera1.enabled = true;
+				Camera2.enabled = false;
+			}
+		} 
+	}
+}
