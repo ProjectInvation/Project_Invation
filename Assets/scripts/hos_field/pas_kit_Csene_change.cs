d1005f3b7722aea72e3ab531c820c5dd2e73d9c3
HOSSY0909 <star.and.river.k.h@i.softbank.jp>
2015-03-21 00:44:39 +0900

    調整

---
+++ Assets/scripts/hos_field/pas_kit_Csene_change.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pas_kit_Csene_change : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider coll)
+	{
+		if(coll.tag==("Pas Scene kitchen Belt"))
+		{
+			switch(coll.tag)
+			{
+			case "Pas Scene kitchen Belt":
+				Application.LoadLevel("kitchen_field");
+				break;
+			}
+		}
+	}
+}
